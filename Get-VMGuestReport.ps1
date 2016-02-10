a91cb1ef58677226a1280aaa27fcff3717bf308e
TechnologyEnthusiast <TechnologyEnthusiast@outlook.com>
2016-02-10 02:39:38 -0800

    Creating version 1 of VM guest report
    I am not sure what information we want to gather for the report, how the report will be delivered, etc.

---
+++ Get-VMGuestReport.ps1
@@ -0,0 +1,69 @@
+#
+# Get-VMGuestReport.ps1
+#
+
+Function Get-VMGuestReport
+{
+	[CmdletBinding()]
+	Param ( 
+		[string[]]$vCenter,
+		[string[]]$VMGuest
+	)
+
+	Begin
+	{
+		Write-Verbose "Adding VMware.VimAutomation.Core snapin and setting powercli configuration"
+		Add-PSSnapin VMware.VimAutomation.Core -ErrorAction SilentlyContinue
+		Set-PowerCLIConfiguration -InvalidCertificateAction 'Ignore' -DefaultVIServerMode 'Multiple' -DisplayDeprecationWarnings:$false -confirm:$false
+		Write-Verbose "Connecting to $vCenter"
+		Connect-VIServer $vCenter
+		$Report = @()
+	}
+
+	Process
+	{
+		write-verbose "Processing stuff"
+		try
+		{
+			foreach ($VM in $VMGuest)
+			{
+				Write-Verbose "Attemping to locate $VM"
+				$Guest = Get-View -ViewType VirtualMachine -Filter @{Name = "$VM"} | Select-Object Name, `
+					@{N="PowerState"; E={$_.Runtime.PowerState}}, `
+					@{N="OS"; E={$_.Config.GuestFullName}}, `
+					@{N="CPU Sockets"; E={$_.Config.Hardware.NumCpu}}, `
+					@{N="CPU Cores"; E={$_.Config.Hardware.NumCoresPerSocket}}, `
+					@{N="Active RAM"; E={$_.Summary.QuickStats.GuestMemoryUsage}}, `
+					@{N="Ballooned RAM"; E={$_.Summary.QuickStats.BalloonedMemory}}, `
+					@{N="Total RAM"; E={$_.Config.Hardware.MemoryMB}}, `
+					@{N="IP"; E={$_.Guest.IpAddress}}, `
+					@{N="Tools Version"; E={$_.Guest.ToolsVersion}}, `
+					@{N="Tools Version Status"; E={$_.Guest.ToolsVersionStatus2}}, ` # use ToolsVersionStatus2, ToolsVersionStatus was deprecated
+					@{N="Tools Running Status"; E={$_.Guest.ToolsRunningStatus}}, `
+					@{N="Notes"; E={$_.Config.Annotation}}, `
+					@{N="HA Protected"; E={$_.Runtime.DasVmProtection.DasProtected}}, `
+					@{N="Snapshot Name"; E={$_.Snapshot.RootSnapshotList.Name}}, `
+					@{N="Snapshot Create Time"; E={$_.Snapshot.RootSnapshotList.CreateTime}}
+					#@{N=""; E={$_.}}, `
+					#@{N=""; E={$_.}}, `
+
+					$Report += $Guest
+			}
+		}
+		catch
+		{
+			Write-Verbose "Caught an exception"
+		}
+	}
+	
+	End
+	{
+		$Date = (Get-Date -Format yyyy-MM-dd.HHmm).toString()
+		$Report | Export-Csv -Path "D:\VMGuest.$Date.csv" -NoTypeInformation
+		$Report
+		#Disconnect-VIServer $vCenter -Confirm:$false
+		write-verbose "The script has finished successfully."
+	}
+}
+
+#Get-VMGuestReport -vCenter $vCenter -VMGuest $VMGuest #-Verbose
